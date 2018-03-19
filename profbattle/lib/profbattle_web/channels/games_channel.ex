bdae32aeafc4a2a668f464ef5ca20ba063ba1a2b
joe <joseph.jeno@gmail.com>
2018-03-19 16:11:25 -0400

    backend

--- profbattle/lib/profbattle_web/channels/games_channel.ex
+++ profbattle/lib/profbattle_web/channels/games_channel.ex
@@ -34,22 +34,23 @@ defmodule ProfbattleWeb.GamesChannel do
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("restart", payload, socket) do
-    game = Game.new()
+  def handle_in("select", %{"professor" => p}, socket) do
+    game = Game.select(socket.assigns[:game], p)
     Profbattle.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" => game}}, socket}
   end
 
-  def handle_in("click", %{"number" => n}, socket) do
-    game = Game.click(socket.assigns[:game], n)
+  def handle_in("attack", %{"player" => n}, socket) do
+    game = Game.attack(socket.assigns[:game], n)
     Profbattle.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" =>  game}}, socket}
   end
 
-  def handle_in("match", payload, socket) do
-    game = Game.match(socket.assigns[:game])
+  # professor is array location
+  def handle_in("swap", %{"player" => n, "professor" => p}, socket) do
+    game = Game.swap(socket.assigns[:game], n, p)
     Profbattle.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" => game}}, socket}
