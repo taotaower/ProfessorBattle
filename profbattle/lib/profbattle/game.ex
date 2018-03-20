5266c29f2da9e3e212fc0b93e6f346ca597dbde8
taotaower <731352521@qq.com>
2018-03-20 15:34:54 -0400

    index works now

--- profbattle/lib/profbattle/game.ex
+++ profbattle/lib/profbattle/game.ex
@@ -61,39 +61,39 @@ defmodule Profbattle.Game do
       end
     end
 
-    def attack(game, player) do
-      clinger = [hp: 3.63, attack: 3.95, defense: 3.95, speed: 3.61, special: 4.03] #1
-      tuck = [hp: 4.37, attack: 4.57, defense: 4.53, speed: 4.23, special: 4.42] #2
-      platt = [hp: 3.93, attack: 4.17, defense: 4.17, speed: 4.25, special: 4.63] #3
-      young = [hp: 4.78, attack: 4.58, defense: 4.84, speed: 4.83, special: 4.87] #4
-      weintraub = [hp: 3.90, attack: 3.25, defense: 4.27, speed: 3.87, special: 4.23] #5
-      derbinsky = [hp: 4.73, attack: 4.10, defense: 4.73, speed: 4.58, special: 4.70] #6
-
-      if player == 1 && game.player2Action == "" do
-        %{
-          gameState: game.gameState,
-          round: game.round,
-          selectingPlayer: game.selectingPlayer,
-          player1: game.player1,
-          player2: game.player2,
-          player1Action: "attack",
-          player2Aaction: game.player2Action
-        }
-      end
-
-      if player == 2 && game.player1Action == "" do
-        %{
-          gameState: game.gameState,
-          round: game.round,
-          selectingPlayer: game.selectingPlayer,
-          player1: game.player1,
-          player2: game.player2,
-          player1Action: game.player1Action
-          player2Aaction: "attack"
-        }
-      end
-
-    end
+#    def attack(game, player) do
+#      clinger = [hp: 3.63, attack: 3.95, defense: 3.95, speed: 3.61, special: 4.03] #1
+#      tuck = [hp: 4.37, attack: 4.57, defense: 4.53, speed: 4.23, special: 4.42] #2
+#      platt = [hp: 3.93, attack: 4.17, defense: 4.17, speed: 4.25, special: 4.63] #3
+#      young = [hp: 4.78, attack: 4.58, defense: 4.84, speed: 4.83, special: 4.87] #4
+#      weintraub = [hp: 3.90, attack: 3.25, defense: 4.27, speed: 3.87, special: 4.23] #5
+#      derbinsky = [hp: 4.73, attack: 4.10, defense: 4.73, speed: 4.58, special: 4.70] #6
+#
+#      if player == 1 && game.player2Action == "" do
+#        %{
+#          gameState: game.gameState,
+#          round: game.round,
+#          selectingPlayer: game.selectingPlayer,
+#          player1: game.player1,
+#          player2: game.player2,
+#          player1Action: "attack",
+#          player2Aaction: game.player2Action
+#        }
+#      end
+#
+#      if player == 2 && game.player1Action == "" do
+#        %{
+#          gameState: game.gameState,
+#          round: game.round,
+#          selectingPlayer: game.selectingPlayer,
+#          player1: game.player1,
+#          player2: game.player2,
+#          player1Action: game.player1Action
+#          player2Aaction: "attack"
+#        }
+#      end
+#
+#    end
 
     def swap(game, player, professor) do
       if player == 1 do
