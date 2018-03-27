f204d7ff22f7daae87dab1a5989d01c91708cb2c
taotaower <731352521@qq.com>
2018-03-26 23:33:27 -0400

    add deployment

--- profbattle/config/prod.exs
+++ profbattle/config/prod.exs
@@ -62,5 +62,5 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
 # import_config "prod.secret.exs"
-config :memory, MemoryWeb.Endpoint,
+config :profbattle, ProfbattleWeb.Endpoint,
        secret_key_base: "THIS+IS+NOT+SECURE+DONT+USE+SESSION+COOKIES+WITHOUT+FIXING"
